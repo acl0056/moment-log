4f7dfb8cfed8c9bd3a87071a2024da341fde5f92
Adam Lockhart <adam.lockhart@goknow.com>
2015-02-24 15:14:42 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function(format) {
+  var moment = require('moment');
+  return function() {
+    var parameters = Array.prototype.slice.call(arguments, 0);
+    parameters.unshift(moment().format(this));
+    console.log.apply(console, parameters);
+  }
+  .bind(format || 'MMMM Do YYYY, h:mm:ss a');
+  // For efficiency, format is bound as "this" so we don't have to 
+  // look in the outer scope for "format".
+}
